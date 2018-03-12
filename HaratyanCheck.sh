117865e4a855aff21bae038bbb58ddb1b6a2bbbf
narugit <naruiroiro@gmail.com>
2018-03-12 10:38:55 +0900

    remove haratyan.html

--- HaratyanCheck.sh
+++ HaratyanCheck.sh
@@ -6,4 +6,5 @@ haratyan_url=`cat ${working_directory}/url.lock`
 wget ${haratyan_url} -O haratyan.html
 nkf -w --overwrite haratyan.html
 python ${working_directory}/main.py
+rm ${working_directory}/haratyan.html
 ${working_directory}/Scheduler.sh
