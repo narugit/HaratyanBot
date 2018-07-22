b8b79500929cc7d787bb60d51dcb61c93e1a6a56
narugit <naruiroiro@gmail.com>
2018-07-22 09:25:45 +0900

    Remove all files.

--- shell/HaratyanCheck.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#Debian用
-working_directory=$(cd $(dirname $0)/..;pwd)
-haratyan_url=`cat ${working_directory}/url.lock`
-wget ${haratyan_url} -O ${working_directory}/haratyan.html
-nkf -w --overwrite ${working_directory}/haratyan.html
-python ${working_directory}/main.py
-${working_directory}/shell/Scheduler.sh
